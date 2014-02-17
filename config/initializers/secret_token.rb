b136439602a25b36b03a405b69f8a9ebdc1a7e05
fdiallo <fallaye.diallo@gmail.com>
2014-02-16 21:17:52 -0800

    Deploying the cs 232 demo app to Github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'a5a265887c4aacdde97ec570af58fec2276325e4fade19c9559312704affdf4995863912464215e19de3ce6ce9a42f98918417fe01f25872681a8e4154eb124e'
